e09d692291b31494af772bf8188f4cc26b81de3f
Todorinski, Stefan <Stefan.Todorinski@adesso.de>
2020-07-01 22:55:40 +0200

    - update

--- main.py
+++ main.py
@@ -388,7 +388,7 @@ def inference(dataset, validation_file, buffer_size, blocks):
             enc_padding_mask = create_mask(x_id)
 
             with tf.GradientTape() as tape:
-                y_hat, y_sp = model(x, enc_padding_mask, x_seg, y_id)
+                y_hat, y_sp = model(x, enc_padding_mask, x_seg, y_id, training=False)
                 loss = loss_function(y_mask, y_w, y_hat, sp, y_sp)
                 mask_accuracy, label_accuracy = accuracy_function(y_mask, y_w, y_hat, sp, y_sp)
 
